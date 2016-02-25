ebf268899a0eea3e2d552ef2a0200c174542fcd1
Joe Longstreet <joelongstreet@gmail.com>
2016-02-25 13:37:53 -0600

    it now has a web and sharing experience

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express   = require('express');
+var path      = require('path');
 var app       = express();
 var server    = require('http').createServer(app);
 var routes    = require('./lib/routes');
@@ -6,6 +7,8 @@ var routes    = require('./lib/routes');
 // setup express
 app.use(express.static('public'));
 server.listen(process.env.PORT || '3002');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // routes
 app.get('/turnt/:id', routes.getTurnt);
